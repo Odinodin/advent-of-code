bf6880be7b59613628c901a73805216819a24ef6
Odin Standal <odin@odinodin.com>
2024-12-02 22:35:21 +0100

    2024 Advent of code star 2

---
+++ clj/2024/src/advent/day_02.clj
@@ -0,0 +1,49 @@
+(ns advent.day-02
+  (:require [advent.util :as util]))
+
+(defn adjacent-diff-in-range? [nums]
+  (every? (fn [[a b]]
+            (let [diff (Math/abs (- a b))]
+              (and (>= diff 1) (<= diff 3))))
+          (partition 2 1 nums)))
+
+(defn strictly-monotonic? [numbers]
+  (or (apply < numbers)
+      (apply > numbers)))
+
+(defn is-safe? [numbers]
+  ;; strictly increasing or decreasing
+  (and (strictly-monotonic? numbers)
+       (adjacent-diff-in-range? numbers)))
+
+(defn remove-one-at-a-time [nums]
+  (map-indexed
+    (fn [idx _]
+      (concat (take idx nums) (drop (inc idx) nums)))
+    nums))
+
+(defn is-actually-safe? [numbers]
+  (some is-safe? (remove-one-at-a-time numbers)))
+
+(defn day-2-star-1 [path]
+  (let [lines (->> (util/read-file-into-list-of-lists path)
+                   (map (fn [line] (map #(Integer/parseInt %) line))))]
+    (->> lines
+         (filter is-safe?)
+         count)))
+
+(defn day-2-star-2 [path]
+  (let [lines (->> (util/read-file-into-list-of-lists path)
+                   (map (fn [line] (map #(Integer/parseInt %) line))))
+        safe-reports (->> lines (filter is-safe?))
+        potentially-safe-reports (->> lines (remove is-safe?) (filter is-actually-safe?))]
+    (->> (concat safe-reports potentially-safe-reports)
+         count)))
+
+(comment
+
+  (day-2-star-1 "resources/day2-test-input.txt")
+  (day-2-star-2 "resources/day2-test-input.txt")
+  (day-2-star-1 "resources/day2-input1.txt")
+  (day-2-star-2 "resources/day2-input1.txt")
+  )
---
+++ clj/2024/src/advent/util.clj
@@ -0,0 +1,10 @@
+(ns advent.util
+  (:require [clojure.java.io :as io])
+  (:require [clojure.string :as str]))
+
+(defn read-file-into-list-of-lists [filepath]
+  (with-open [rdr (io/reader filepath)]
+    (doall
+      (map
+        #(str/split % #"\s+")
+        (line-seq rdr)))))
