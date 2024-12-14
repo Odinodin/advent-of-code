f164854f3b6113ae128e6f8542d8d0200d1f40a9
Odin Standal <odin@odinodin.com>
2024-12-14 14:16:50 +0100

    2024 Advent of code day 8 star 2

--- clj/2024/src/advent/day_08.clj
+++ clj/2024/src/advent/day_08.clj
@@ -22,21 +22,19 @@
                                                 (map (fn [list-of-k-v] (map (fn [x] (first x)) list-of-k-v))))
         pairs-of-same-antenna-coords (->> list-of-lists-with-xy-of-same-char
                                           (mapcat (fn [list-with-xy-of-same-char]
-                                                    (combo/combinations list-with-xy-of-same-char 2)
-                                                    )))
+                                                    (combo/combinations list-with-xy-of-same-char 2))))
         [width height] (util/bounds-map xy-char)]
     (let [antinode-coords (->>
                             pairs-of-same-antenna-coords
-                            (mapcat (fn [foo]
-                                      (let [[x1 y1] (first foo)
-                                            [x2 y2] (second foo)
+                            (mapcat (fn [two-pairs]
+                                      (let [[x1 y1] (first two-pairs)
+                                            [x2 y2] (second two-pairs)
                                             antinode-1-x (+ x1 (- x1 x2))
                                             antinode-1-y (+ y1 (- y1 y2))
                                             antinode-2-x (+ x2 (- x2 x1))
                                             antinode-2-y (+ y2 (- y2 y1))]
                                         [[antinode-1-x antinode-1-y]
-                                         [antinode-2-x antinode-2-y]
-                                         ])))
+                                         [antinode-2-x antinode-2-y]])))
                             distinct
                             (remove #(out-of-bounds? [width height] %)))]
 
@@ -57,17 +55,43 @@
              (util/print-map!))
       (count antinode-coords))))
 
-(defn day-8-star-2 [path]
+(defn antinode-next-to [[x y] delta-x delta-y]
+  [(+ x delta-x) (+ y delta-y)])
 
-  )
+(defn day-8-star-2 [path]
+  (let [xy-char (util/xy-to-char path)
+        list-of-lists-with-xy-of-same-char (->> (group-by second xy-char)
+                                                (m/remove-keys #(= \. %))
+                                                vals
+                                                (map (fn [list-of-k-v] (map (fn [x] (first x)) list-of-k-v))))
+        pairs-of-same-antenna-coords (->> list-of-lists-with-xy-of-same-char
+                                          (mapcat (fn [list-with-xy-of-same-char]
+                                                    (combo/combinations list-with-xy-of-same-char 2))))
+        [width height] (util/bounds-map xy-char)]
+    (let [antinode-coords (->>
+                            pairs-of-same-antenna-coords
+                            (mapcat (fn [two-pairs]
+                                      (let [[x1 y1] (first two-pairs)
+                                            [x2 y2] (second two-pairs)]
+                                        (concat (->> (iterate inc 0)
+                                                     (map (fn [i]
+                                                            (antinode-next-to [x1 y1] (* i (- x1 x2)) (* i (- y1 y2)))))
+                                                     (take-while (fn [coord]
+                                                                   (not (out-of-bounds? [width height] coord)))))
+                                                (->> (iterate inc 0)
+                                                     (map (fn [i]
+                                                            (antinode-next-to [x2 y2] (* i (- x2 x1)) (* i (- y2 y1)))))
+                                                     (take-while (fn [coord]
+                                                                   (not (out-of-bounds? [width height] coord)))))))))
+                            distinct
+                            (remove #(out-of-bounds? [width height] %)))]
+      (count antinode-coords))))
 
 (comment
-
-
-
 
   (day-8-star-1 "resources/day08/input-test.txt")
   (day-8-star-1 "resources/day08/input.txt")
 
-  (day-8-star-2 "resources/day08/-test-input.txt")
-  (day-8-star-2 "resources/day08/-input.txt"))
+  (day-8-star-2 "resources/day08/input-test.txt")
+  (day-8-star-2 "resources/day08/input.txt")
+  )
