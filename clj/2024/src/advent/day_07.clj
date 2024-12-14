ac7fbbd4735d90b37fbec65bd1ad78308327dc7f
Odin Standal <odin@odinodin.com>
2024-12-14 13:34:58 +0100

    2024 Advent of code day 7

---
+++ clj/2024/src/advent/day_07.clj
@@ -0,0 +1,71 @@
+(ns advent.day-07
+  (:require [advent.util :as util]
+            [clojure.string :as str]
+            [medley.core :as m]))
+
+
+(defn all-operator-combinations
+  "Returns a lazy sequence of all combinations of the items in operators of length x."
+  [operators n]
+  (if (zero? n)
+    '(())
+    (for [rest (all-operator-combinations operators (dec n))
+          operator operators]
+      (conj rest operator))))
+
+
+(comment
+  (all-operator-combinations 3)
+  )
+
+(defn execute [numbers operators]
+  (let [math-to-solve-left-to-right (m/interleave-all numbers operators)]
+    (reduce (fn [acc [op x]]
+              (cond  (= :times op)
+                     (* acc x)
+
+                     (= :plus op) (+ acc x)
+
+                     (= :concat op) (Long/parseLong (str acc x))
+                     ))
+            (first math-to-solve-left-to-right)
+            (partition 2 (rest math-to-solve-left-to-right)))))
+
+
+(defn does-compute? [operators {:keys [desired-result nums]}]
+  (let [combinations (all-operator-combinations operators (count nums))]
+    (some (fn [combination]
+            (let [actual (execute nums combination)]
+              (= desired-result actual)))
+      combinations)))
+
+(defn day-7-star-1 [path]
+  (let [lines (util/read-file-into-list path)
+        sum-and-input (->> lines
+                           (map (fn [line] (let [[sum input] (str/split line #":")]
+                                             {:desired-result (Long/parseLong sum)
+                                              :nums (mapv Long/parseLong (-> (str/trim input)
+                                                                             (str/split #"\s")))}))))]
+    (->> sum-and-input
+         (filter #(does-compute? [:times :plus] %))
+         (map :desired-result)
+         (reduce +))))
+
+(defn day-7-star-2 [path]
+  (let [lines (util/read-file-into-list path)
+        sum-and-input (->> lines
+                           (map (fn [line] (let [[sum input] (str/split line #":")]
+                                             {:desired-result (Long/parseLong sum)
+                                              :nums (mapv Long/parseLong (-> (str/trim input)
+                                                                             (str/split #"\s")))}))))]
+    (->> sum-and-input
+         (filter #(does-compute? [:times :plus :concat] %))
+         (map :desired-result)
+         (reduce +))))
+
+(comment
+  (day-7-star-1 "resources/day-7-test-input.txt")
+  (day-7-star-1 "resources/day-7-input.txt")
+
+  (day-7-star-2 "resources/day-7-test-input.txt")
+  (day-7-star-2 "resources/day-7-input.txt"))
