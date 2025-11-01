42fb684b5b1486afe299f4811d46003c25a350ae
Odin Standal <odin@odinodin.com>
2025-11-01 10:43:15 +0100

    2024 Kotlin: Solve day 1

--- kotlin/src/main/kotlin/aoc2024/Day01.kt
+++ kotlin/src/main/kotlin/aoc2024/Day01.kt
@@ -2,22 +2,41 @@ package aoc2024
 
 import resourceAsListOfString
 
-fun solveDay01(inputPath: String) {
+fun solveDay01Part1(inputPath: String) {
   val lines = resourceAsListOfString(inputPath)
 
-  val output = lines.map {
-    it.split("\\s+".toRegex()).map { it.toInt() }
-  }
+  val (left, right) = lines.map {
+    val (a, b) = it.split("\\s+".toRegex()).map { it.toInt() }
+    Pair(a, b)
+  }.unzip()
 
-
-
-  println(output)
-
+  left.sorted()
+    .zip(right.sorted())
+    .map { Math.abs(it.first - it.second) }
+    .sum()
+    .also { println(it) }
 }
+
+fun solveDay01Part2(inputPath: String) {
+  val lines = resourceAsListOfString(inputPath)
+
+  val (left, right) = lines.map {
+    val (a, b) = it.split("\\s+".toRegex()).map { it.toInt() }
+    Pair(a, b)
+  }.unzip()
+
+  left.map { curr -> right.count { curr == it } * curr }
+    .sum()
+    .also { println(it) }
+}
+
 
 fun main() {
 
-  solveDay01("2024/day01.txt")
+  solveDay01Part1("2024/day01_sample.txt")
+  solveDay01Part1("2024/day01.txt")
 
+  solveDay01Part2("2024/day01_sample.txt")
+  solveDay01Part2("2024/day01.txt")
 
 }
