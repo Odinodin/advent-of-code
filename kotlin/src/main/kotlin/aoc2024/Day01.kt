90b42888a56ca67a02398ba6b28fbb2ab0fbfcf6
Odin Standal <odin@odinodin.com>
2025-11-01 10:53:13 +0100

    2024 Kotlin: Improve day 1

--- kotlin/src/main/kotlin/aoc2024/Day01.kt
+++ kotlin/src/main/kotlin/aoc2024/Day01.kt
@@ -6,13 +6,13 @@ fun solveDay01Part1(inputPath: String) {
   val lines = resourceAsListOfString(inputPath)
 
   val (left, right) = lines.map {
-    val (a, b) = it.split("\\s+".toRegex()).map { it.toInt() }
-    Pair(a, b)
+    val (a, b) = it.split(Regex("\\s+")).map { it.toLong() }
+    a to b // Pair
   }.unzip()
 
   left.sorted()
     .zip(right.sorted())
-    .map { Math.abs(it.first - it.second) }
+    .map { (first, second) -> Math.abs(first - second) }
     .sum()
     .also { println(it) }
 }
@@ -21,22 +21,27 @@ fun solveDay01Part2(inputPath: String) {
   val lines = resourceAsListOfString(inputPath)
 
   val (left, right) = lines.map {
-    val (a, b) = it.split("\\s+".toRegex()).map { it.toInt() }
-    Pair(a, b)
+    val (a, b) = it.split("""\s+""".toRegex()).map { it.toLong() }
+    a to b // Pair
   }.unzip()
 
-  left.map { curr -> right.count { curr == it } * curr }
-    .sum()
-    .also { println(it) }
+  // Calculate how many times each number appears in the left list
+  // This version runs through the right list for each number, not very efficient
+//  left.map { curr -> right.count { curr == it } * curr }
+//    .sum()
+//    .also { println(it) }
+
+  // Note the use of groupingBy, which is not the same as groupBy.
+  // A Grouping is a construct that supports certain operations per group, like fold or eachCount.s
+  val freqs = right.groupingBy { it }.eachCount()
+  left.map { freqs.getOrDefault(it, 0) * it }.sum().also { println(it) }
 }
 
 
 fun main() {
-
   solveDay01Part1("2024/day01_sample.txt")
   solveDay01Part1("2024/day01.txt")
 
   solveDay01Part2("2024/day01_sample.txt")
   solveDay01Part2("2024/day01.txt")
-
 }
